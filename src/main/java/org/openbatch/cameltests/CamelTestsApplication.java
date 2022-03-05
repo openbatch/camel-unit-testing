1b5863e902d84617a9a864bd8284f44c088219c7
Larry Shields <larry.shields@gmail.com>
2022-03-05 15:43:27 -0500

    Project Setup

---
+++ src/main/java/org/openbatch/cameltests/CamelTestsApplication.java
@@ -0,0 +1,13 @@
+package org.openbatch.cameltests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/openbatch/cameltests/CamelTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openbatch.cameltests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
